e67068c41ca284ea18e8cf0f407a7fb08415514b
t1k2a <maxtj1122@gmail.com>
2026-01-30 18:16:09 +0900

    Amplify のAI実装をしてみる

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Amplify } from 'aws-amplify';
+import '@aws-amplify/ui-react/styles.css';
+import outputs from '../../amplify_outputs.json';
+
+Amplify.configure(outputs);
 
 createRoot(document.getElementById("root")!).render(<App />);
